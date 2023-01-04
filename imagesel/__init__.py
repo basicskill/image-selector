d9ee749cb851b18c1b5205aa95bb105f3d8a41b2
Mladen Basic <mladenbasic99@gmail.com>
2023-01-04 15:08:13 +0100

    debugging .run()

--- imagesel/__init__.py
+++ imagesel/__init__.py
@@ -17,18 +17,18 @@ def create_app(*args, **kwargs):
         # DATABASE=os.path.join(app.instance_path, 'imagedb.sqlite'),
     )
 
-    # if test_config is None:
-    #     # load the instance config, if it exists, when not testing
-    #     app.config.from_pyfile('config.py', silent=True)
-    # else:
-    #     # load the test config if passed in
-    #     app.config.from_mapping(test_config)
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_myfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
 
-    # # ensure the instance folder exists
-    # try:
-    #     os.makedirs(app.instance_path)
-    # except OSError:
-    #     pass
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
 
     # a simple page that says hello
     @app.route('/hello')
@@ -74,3 +74,7 @@ def create_app(*args, **kwargs):
     # Define number of correct images to label to proceed testing
     app.config["NUM_CORRECT"] = 1
     return app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run()
