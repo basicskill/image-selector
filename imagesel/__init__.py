142f158364c69b0f76f423656babe746d194b25e
Mladen Basic <mladenbasic99@gmail.com>
2023-01-04 14:33:56 +0100

    Heroku testing.

--- imagesel/__init__.py
+++ imagesel/__init__.py
@@ -4,6 +4,8 @@ from flask import Flask, render_template, g, url_for, redirect
 import psycopg2
 
 def create_app(test_config=None):
+
+    print("App started")
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
@@ -11,24 +13,24 @@ def create_app(test_config=None):
         # DATABASE=os.path.join(app.instance_path, 'imagedb.sqlite'),
     )
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+    # if test_config is None:
+    #     # load the instance config, if it exists, when not testing
+    #     app.config.from_pyfile('config.py', silent=True)
+    # else:
+    #     # load the test config if passed in
+    #     app.config.from_mapping(test_config)
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+    # # ensure the instance folder exists
+    # try:
+    #     os.makedirs(app.instance_path)
+    # except OSError:
+    #     pass
 
     # a simple page that says hello
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
-    
+    print("Generated index page")
     # Init database
     from . import db
     db.init_app(app)
